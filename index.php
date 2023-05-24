1153a0fe859fcb6a2b5d972f591a13ea003c45c5
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-24 14:21:10 +0900

    Layout cards in progress

--- index.php
+++ index.php
@@ -6,13 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Link a Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Link al foglio di stile -->
+    <link rel="stylesheet" href="./style.css">
     <title>Dischi JSON</title>
 </head>
 <body>
     <div id="vue_app" class="vh-100">
-        <header>
+        <header class="d-flex justify-content-center align-items-center position-relative">
+            <img src="./logo-small.svg" alt="logo">
             <h1 class="text-center text-primary">Spotify Records</h1>
         </header>
+        <main class="d-flex justify-content-center align-items-start pt-4">
+            <div id="card_set" class="w-75 row flex-wrap justify-content-between align-items-center">
+                <div class="card"
+                 v-for="(item, index) in api_data"
+                 :key="index"
+                >
+                    <img :src="item.poster" class="card-img-top h-50" :alt="item.author">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick ehe card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
     <!-- CDN Bootstrap 5 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
